86482f19c3a8356bb9fa216f4fc6bd41ff2ca890
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2026-02-13 03:30:10 +0300

    feat: implement encryption key management and session storage

--- electron/main.ts
+++ electron/main.ts
@@ -1,13 +1,18 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
 import { LoginRequest, LoginResponse, Session, SessionResponse } from "./types/auth";
-
+import keytar from 'keytar'
+import crypto from "crypto";
+import Store from 'electron-store'
 const isDev = !app.isPackaged;
-
+const SERVICE_NAME ="proctor-app"
+const ACCOUNT_NAME="proctor-encryption-key"
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1100,
-    height: 800,
+    width: 1920,
+    height: 1080,
+    fullscreen:false,
+    autoHideMenuBar:true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
@@ -24,9 +29,33 @@ function createWindow() {
     );
   }
 }
+async function getEncryptionKey():Promise<string>{
+  let key = await keytar.getPassword(SERVICE_NAME,ACCOUNT_NAME)
+  if (!key) {
+    key=crypto.randomBytes(32).toString("hex")
+    await keytar.setPassword(SERVICE_NAME,ACCOUNT_NAME,key)
+  }
+  return key
+}
+let store:Store
+app.whenReady().then(async () => {
+  const encryptionKey = await getEncryptionKey();
+
+  store = new Store({
+    name: "proctor-session",
+    encryptionKey
+  });
+
+  const savedSession = store.get("session");
+
+  if (savedSession) {
+    currentSession = savedSession as Session;
+  }
+
+  createWindow();
+});
 
 let currentSession:Session | null = null
-app.whenReady().then(createWindow);
 ipcMain.handle(
   "auth:login",
   async (_, credentials: LoginRequest): Promise<LoginResponse> => {
